94e412e198df48ae9e63581be2e4a7aafbf787e2
Pogorielov Sergii <pogorelov.ss@gmail.com>
2019-11-28 10:48:25 +0200

    wip Added Encouter Procedure Observation

--- README.md
+++ README.md
@@ -7,8 +7,14 @@ python  command line utility ... test task for this year
 
 * [Click](https://click.palletsprojects.com/en/7.x/)
 * [sqlalchemy](https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91)
+* [alembic](https://alembic.sqlalchemy.org/)
 * [jsonlines](https://jsonlines.readthedocs.io/en/latest/)
 
+## Task description
+
+* [Task 1](https://docs.google.com/document/d/1FwndaKyc3Ua8z0tJTnv34nf3Ass4VigaemGeGNDXkGA/edit?usp=sharing)
+* `напишите консольную программу которая будет обходить матрицу улиткой начиная с левого верхнего угла. На входе, параметрами 2 числа, размерность матрицы IxJ, на выходе список текущих координат для каждой посещенной точки. Например:  visit.py 3x3  0,0  0,1  0,2  1,2  2,2  2,1  2,0  1,0  1,1`
+* `напишите консольную программу которая по заданному числу находит обратное ему бинарное число (13 => 1101, обратное ему (читаем справа налево) 1011 => 11). На входе параметром число N 1<= N <= 1000000000, на выходе обратное число    reverse_bin.py 13    11`
 
 ### credits
 
--- testtask112019/fhir.py
+++ testtask112019/fhir.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, DateTime, Numeric
+from sqlalchemy.orm import sessionmaker, relationship
 
 
 engine = create_engine('sqlite:///:sqlite.db', echo=True)
@@ -10,9 +10,11 @@ session = Session()
 
 Base = declarative_base()
 
+# TODO: custom class with Columns: id, type_code, type_code_system?
+
 class Patient(Base):
     __tablename__ = 'patients'
-    # __tablename__ = self.__name__.lower()
+
     id = Column(Integer, primary_key=True)
     source_id = Column(String)
     birth_date = Column(Date)
@@ -22,3 +24,50 @@ class Patient(Base):
     ethnicity_code = Column(String)
     ethnicity_code_system = Column(String)
     country = Column(String)
+
+
+class Encouter(Base):
+    __tablename__ = 'encouters'
+
+    id = Column(Integer, primary_key=True)
+    source_id = Column(Integer)
+    patient_id = Column(Integer, ForeignKey('patients.id'))
+    start_date = Column(DateTime)
+    end_date = Column(DateTime)
+    type_code = Column(String)
+    type_code_system = Column(String)
+
+Patient.encouters = relationship('Encouter', order_by=Encouter.id, back_populates="patient")
+
+
+class Procedure(Base):
+    __tablename__ = 'procedures'
+
+    id = Column(Integer, primary_key=True)
+    source_id = Column(Integer)
+    patient_id = Column(Integer, ForeignKey('patients.id'))
+    encouter_id = Column(Integer, ForeignKey('encouters.id'))
+    procedure_date = Column(Date)
+    type_code = Column(String)
+    type_code_system = Column(String)
+
+Patient.procedures = relationship('Procedure', order_by=Procedure.id, back_populates="patient")
+Encouter.procedures = relationship('Procedure', order_by=Procedure.id, back_populates="encouter")
+
+
+class Observation(Base):
+    __tablename__ = 'observations'
+
+    id = Column(Integer, primary_key=True)
+    source_id = Column(Integer)
+    patient_id = Column(Integer, ForeignKey('patients.id'))
+    encouter_id = Column(Integer, ForeignKey('encouters.id'))
+    observation_date = Column(Date)
+    type_code = Column(String)
+    type_code_system = Column(String)
+    value = Column(Numeric) # TODO: extra params?
+    unit_code = Column(String)
+
+
+Patient.observations = relationship('Observation', order_by=Observation.id, back_populates="patient")
+Encouter.observations = relationship('Observation', order_by=Observation.id, back_populates="encouter")
